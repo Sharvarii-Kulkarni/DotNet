3592c01d7f748ebf7fca0812f0bd56eb5a1b8bcc
sharvarikulkarni30 <ksharvari2003@gmail.com>
2025-04-06 18:28:23 +0530

    College Fest App

---
+++ CollegeFest/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CollegeFest
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPeople;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblCatering;
+        private System.Windows.Forms.Label lblDecoration;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPeople = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblCatering = new System.Windows.Forms.Label();
+            this.lblDecoration = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number of Students:";
+            // 
+            // txtPeople
+            // 
+            this.txtPeople.Location = new System.Drawing.Point(250, 30);
+            this.txtPeople.Name = "txtPeople";
+            this.txtPeople.Size = new System.Drawing.Size(150, 26);
+            this.txtPeople.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(100, 80);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(150, 35);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Start Fest";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblCatering
+            // 
+            this.lblCatering.AutoSize = true;
+            this.lblCatering.Location = new System.Drawing.Point(30, 140);
+            this.lblCatering.Name = "lblCatering";
+            this.lblCatering.Size = new System.Drawing.Size(165, 20);
+            this.lblCatering.TabIndex = 3;
+            this.lblCatering.Text = "Catering Charges: Rs.";
+            // 
+            // lblDecoration
+            // 
+            this.lblDecoration.AutoSize = true;
+            this.lblDecoration.Location = new System.Drawing.Point(12, 180);
+            this.lblDecoration.Name = "lblDecoration";
+            this.lblDecoration.Size = new System.Drawing.Size(183, 20);
+            this.lblDecoration.TabIndex = 4;
+            this.lblDecoration.Text = "Decoration Charges: Rs.";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(450, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPeople);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblCatering);
+            this.Controls.Add(this.lblDecoration);
+            this.Name = "Form1";
+            this.Text = "College Fest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ CollegeFest/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CollegeFest
+{
+    public partial class Form1 : Form
+    {
+        // Delegate and event
+        public delegate void FestHandler(int peopleCount);
+        public event FestHandler CollegeFest;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Subscribe Decorator and Caterer to the event with labels
+            CollegeFest += new Decorator(lblDecoration).OnCollegeFest;
+            CollegeFest += new Caterer(lblCatering).OnCollegeFest;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numberOfPeople = int.Parse(txtPeople.Text);
+                CollegeFest?.Invoke(numberOfPeople); // Trigger event
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+
+    // Caterer class (event subscriber)
+    public class Caterer
+    {
+        private Label label;
+        public Caterer(Label lbl)
+        {
+            label = lbl;
+        }
+
+        public void OnCollegeFest(int people)
+        {
+            int charges = people * 200;
+            label.Text = $"Catering Charges: Rs. {charges}";
+        }
+    }
+
+    // Decorator class (event subscriber)
+    public class Decorator
+    {
+        private Label label;
+        public Decorator(Label lbl)
+        {
+            label = lbl;
+        }
+
+        public void OnCollegeFest(int people)
+        {
+            int charges = 10000 + (people * 10);
+            label.Text = $"Decoration Charges: Rs. {charges}";
+        }
+    }
+}
